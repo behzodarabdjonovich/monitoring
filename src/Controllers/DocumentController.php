d1ff3019bb0fd60efc87c32d0c61883991ce365b
behzodarabdjonovich <162014553+behzodarabdjonovich@users.noreply.github.com>
2026-09-11 10:14:20 +0500

    Update DocumentController.php

--- src/Controllers/DocumentController.php
+++ src/Controllers/DocumentController.php
@@ -229,10 +229,8 @@ if (DB::driver() === 'pgsql') {
     }
 }
         
-        $fileRow = DB::selectOne(
-  if (DB::driver() === 'pgsql') {
-    $fileRow = DB::selectOne(
-        "SELECT encode(file_data, 'base64') AS file_data_b64
+        if (DB::driver() === 'pgsql') {
+          "SELECT encode(file_data, 'base64') AS file_data_b64
          FROM documents
          WHERE id = :id",
         ['id' => $id]
@@ -248,8 +246,7 @@ if (DB::driver() === 'pgsql') {
         }
     }
 } else {
-    $fileRow = DB::selectOne(
-        "SELECT file_data
+          "SELECT file_data
          FROM documents
          WHERE id = :id",
         ['id' => $id]
